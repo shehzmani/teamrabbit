d3e5c2b71fee357b3997f2d29431dabd54217cf9
ZerenDuan <80794002+ZerenDuan@users.noreply.github.com>
2021-12-09 02:10:26 -0800

    Add files via upload

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 # Create your models here.
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        max_length=30,
+        unique= True,
+        validators=[RegexValidator(
+            regex=r'^@\w{3,}$',
+            message='username must consist of @ followed by at least 3 alphanumericals'
+        )]
+    )
+    first_name = models.CharField(label='First name',max_length=50)
+    last_name = models.CharField(label='last name',max_length=50)
+    username = models.CharField(label ='username',max_length=50)
+    email = models.EmailField(label='Email')
