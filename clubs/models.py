4877fa4e1900e74a62ab8678c8a3e5bc48e64c51
Shehzmani Shams <shehzmani.shams@kcl.ac.uk>
2021-12-10 23:12:11 +0000

    Add user model and make migrations

--- clubs/models.py
+++ clubs/models.py
@@ -12,12 +12,13 @@ class User(AbstractUser):
     #message='Username must consist of an @ followed by atleast seven alphanumericals'
     #)]
     #)
+    )
 
-    first_name = models.CharField(blank=False, max_length=50)
+    first_name = models.CharField(blank=False, max_length=50),
 
-    last_name = models.CharField(blank=False, max_length=50)
+    last_name = models.CharField(blank=False, max_length=50),
 
-    email=models.EmailField(blank=False, unique=True)
+    email=models.EmailField(blank=False, unique=True),
 
     bio = models.CharField(blank= True, max_length=520)
 
--- clubs/views.py
+++ clubs/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+def home(request):
+    return render(request, 'home.html')
+    
 def sign_up(request):
     return render(request, 'sign_up.html')
